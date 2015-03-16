bb24ccf03c2fb43770f6482a578be3e8dbc10e99
15inchdesign <15inchdesignes@gmail.com>
2015-03-16 15:11:36 +0100

    updates
    small updates

--- js/animation.js
+++ js/animation.js
@@ -4,53 +4,89 @@
  * and open the template in the editor.
  */
 //$(window).load(function(){
- 
+
 //});
 //function showME(){
-    //priljepljeni GIF na html 
-    // body je skriveni
-    //postavljamo funkciju koliko zadržavamo/sakrivamo funkciju 
- // $('html').show().append('<img id="preload" src="img/anim-logo.gif" alt="BOONER" />');  
+        //priljepljeni GIF na html 
+        // body je skriveni
+        //postavljamo funkciju koliko zadržavamo/sakrivamo funkciju 
+        // $('html').show().append('<img id="preload" src="img/anim-logo.gif" alt="BOONER" />');  
 //};
 //setTimeout(showME, 500);
 
 //function takeME (){
-    // sakrivamo GIF sa smooth animacijom pomoću delay() funkcije
-    // i video u kombinaciji timera
-   // $('#preload').delay(3500).hide(500);
-   // $('video').hide();
+        // sakrivamo GIF sa smooth animacijom pomoću delay() funkcije
+        // i video u kombinaciji timera
+        // $('#preload').delay(3500).hide(500);
+        // $('video').hide();
 //};
 //setTimeout(takeME, 3600);
 
 //function blowME (){
-    // fadeIn funkciojm  polagano prikazivamo <body></body>
-    // sa delay funkcijom odgađamo vreme da dobimo smooth efekt
-    // isto vrijedi i za video class="intro"
-   // $('body').fadeIn(2000);
-   //  $('.intro').delay(2500).fadeIn(2800);       
+        // fadeIn funkciojm  polagano prikazivamo <body></body>
+        // sa delay funkcijom odgađamo vreme da dobimo smooth efekt
+        // isto vrijedi i za video class="intro"
+        // $('body').fadeIn(2000);
+        //  $('.intro').delay(2500).fadeIn(2800);       
 //};
 //setTimeout(blowME, 8500);
 
 //verzija 2
-$(function(){
-    $('<img id="preload" src="img/anim-logo.gif" alt="BOONER" />').insertBefore('body').animate({
-      'display' : 'block'
-       },{
-           duration:'slow',
-           queue: true,
-           step:function (){
-               $('#preload').delay(8000).hide(500); 
-               $('video').hide();
-               queue: false;
-             }
-           });
-           $('body').delay(10000).show(400);
-           $('.intro').delay(10500).fadeIn(2800);
-       });
-      
-
-$(function(){
+        $(function (e) {
+            $('<img id="preload" src="img/anim-logo.gif" alt="BOONER" />').insertBefore('body').animate({
+                'display': 'block'
+            }, {
+                duration: 'slow',
+                queue: false,
+                step: function () {
+                    $('#preload').delay(8000).hide(500);
+                    $('video').hide();
+                    queue: false;
+                }
+            });
+            $('body').delay(10000).show(400);
+            $('.intro').delay(10500).fadeIn(2800);
+			//$('html').before('body').unbind('<img id="preload" src="img/anim-logo.gif" alt="BOONER" />');
+        });//html destroy after load images
+$(function () {
     $('h1').addClass('animated fadeInDownBig');
     $('h4').addClass('animated flipInY');
 });
+//$(function(){
+		//$('html').remove('<img id="preload" src="img/anim-logo.gif" alt="BOONER" />');
+//});
+
+$(function () {
+    $('.push').click(function () {
+        $('#form').addClass('animated rollIn').css({
+            'display': 'block'
+        });
+    });
+    $(function () {
+        $('#close').click(function () {
+            //close.preventDefault();
+            //$('#form').addClass('animated zoomOut');
+                   // $(this).css({'display': 'none'});
+            $('#form').slideUp(2000);
+          //  close.preventDefault();
+
+        });
+    });
+	// parllax
+         $(function () {
+		 $('section.form-1').stellar({
+		 horizontalOffset: 0,
+		 verticalOffset: 0,
+		 horizontalScrolling: true,
+		 verticalScrolling: true,
+		 scrollProperty: 'transform'
+		 });
+	});
+});
+
+        
+
+       
+   
+
 
